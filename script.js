56ed09dffc0450ef98b369403792a73b5f8b28b2
AndrewSimons3 <simdrew07@gmail.com>
2022-08-09 13:28:08 -0700

    finished javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const form = document.querySelector('.email-form');
+const input = document.querySelector('.input');
+const error = document.querySelector('.error');
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const emailVal = input.value;
+
+  if (!validateEmail(emailVal)) {
+    error.classList.add('active-error-state');
+    input.classList.add('active-error-border-state')
+  }
+});
+
+form.addEventListener('keyup', () => {
+  error.classList.remove('active-error-state');
+})
+
+
+const validateEmail = (email) => {
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+};
